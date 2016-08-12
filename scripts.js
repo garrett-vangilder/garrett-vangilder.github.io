7da8e804537924ed9218bef7d56f19dc74417435
Garrett Vangilder <garrett.vangilder@gmail.com>
2016-08-12 14:24:55 -0500

    added a few features

--- scripts.js
+++ scripts.js
@@ -1,1 +1,19 @@
+$(document).ready(function() {
+
 console.log('This is being built')
+
+// SPA Functionality
+$('.aboutButton').on('click', function() {
+  console.log('about was clicked')
+  $('')
+})
+
+$('.portfolioButton').on('click', function() {
+  console.log('portfolio was clicked')
+})
+
+$('.contactButton').on('click', function() {
+  console.log('contact was clicked')
+})
+
+})
