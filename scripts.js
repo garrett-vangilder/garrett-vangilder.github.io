2351e38536e726197d079982085e95b7c53a6b58
Garrett Vangilder <garrett.vangilder@gmail.com>
2016-09-08 18:06:44 -0500

    finished

--- scripts.js
+++ scripts.js
@@ -1,25 +1,31 @@
 $(document).ready(function() {
 
-console.log('This is being built')
+    console.log('This is being built')
 
-// SPA Functionality
-$('.aboutButton').on('click', function() {
-  console.log('about was clicked')
-  $('')
-})
+    // SPA Functionality
+    $('.aboutButton').on('click', function() {
+        console.log('about was clicked')
+        $('')
+    })
 
-$('.portfolioButton').on('click', function() {
-  console.log('portfolio was clicked')
-})
+    $('.portfolioButton').on('click', function() {
+        console.log('portfolio was clicked')
+    })
 
-$('.contactButton').on('click', function() {
-  console.log('contact was clicked')
-})
+    $('.contactButton').on('click', function() {
+        console.log('contact was clicked')
+    })
 
-  $(".annieButton").on("click", function() {
-    $(".annieButton").hide();
-    $(".annieContent").removeClass('hidden');
-    console.log("Annie's Button")
-  })
+    $(".annieButton").on("click", function() {
+        $(".annieButton").hide();
+        $(".annieContent").removeClass('hidden');
+        console.log("Annie's Button")
+    })
+
+    $(".giftButton").on("click", () => {
+        $(".gifts").removeClass("hidden");
+        $(".giftButton").hide();
+
+    })
 
 })
